f1a41cb64c2b81bf2f6fabcaddce933b0e2a2231
Bruce Chou <bruce@vynca.org>
2017-05-13 01:22:43 +0800

    Add preprocessing to lib

--- bruce/js/lib/index.js
+++ bruce/js/lib/index.js
@@ -1,7 +1,9 @@
 import metrics from './metrics';
 import linearModel from './linear_model';
+import preprocessing from './preprocessing';
 
 export default {
     metrics,
     linearModel,
+    preprocessing,
 }

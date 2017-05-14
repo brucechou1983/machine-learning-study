2770acc0339b1abba8215ecb7afe3c2baf50e0bc
Bruce Chou <bruce@vynca.org>
2017-05-14 17:32:53 +0800

    Add max PLA iteration estimation to Pocket

--- bruce/js/lib/linear_model/pocket.js
+++ bruce/js/lib/linear_model/pocket.js
@@ -8,6 +8,55 @@ export default class PocketPLA extends Perceptron {
     super(verbose);
   }
 
+  _countMaxPLAIter(x, y, sampleRatio = 0.1) {
+    // theoretically the maximum iterations for PLA to converge
+    // T = ((max ||x||)^2 / (min distance of x to perfect model)^2
+    // T can't be calculated since we don't have that perfect model
+    // however we can randomly sample some (+1) and (-1) examples
+    // and calculate a approximation of T
+
+    const maxSampleIter = 1000;
+    let sampleIter = 0;
+    let groupedSamples = [];
+
+    // count max length of sampled x
+    let maxVectorLength = 0;
+    _.map(x, (sampleX) => {
+      if (sampleX.magnitude() > maxVectorLength) {
+        maxVectorLength = sampleX.magnitude();
+      }
+    });
+    if (this.verbose_ > 0) {
+      console.log(`max vector length: ${maxVectorLength}`);
+    }
+
+    // group samples
+    while (groupedSamples.length !== 2 && sampleIter < maxSampleIter) {
+      const samples = _.sampleSize(_.zip(x, y), Math.floor(sampleRatio * x.length));
+      groupedSamples = _.partition(samples, (sample) => sample[1] > 0);
+      sampleIter += 1;
+    }
+
+    // count min distance between grouped samples
+    let minGroupedSampleDist = Number.MAX_SAFE_INTEGER;
+    _.map(groupedSamples[0], (p) => {
+      _.map(groupedSamples[1], (n) => {
+        const dist = p[0].subtract(n[0]).magnitude();
+        if (dist < minGroupedSampleDist) {
+          minGroupedSampleDist = dist;
+        }
+      })
+    });
+    if (this.verbose_ > 0) {
+      console.log(`min grouped samples distance: ${minGroupedSampleDist}`);
+    }
+
+    const maxPLAIter = Math.floor(Math.pow(maxVectorLength, 2) / Math.pow(minGroupedSampleDist / 2, 2));
+    console.log(`max PLA iterations: ${maxPLAIter}`);
+
+    return maxPLAIter;
+  }
+
   _countError(x, y, w, sample) {
     let errorCounts = 0;
     if (!sample || sample <= 0) {
@@ -28,7 +77,7 @@ export default class PocketPLA extends Perceptron {
   }
 
   fit({ x, y, initWeights, sample, maxIter=100000 } = {}) {
-    if (this.verbose_) {
+    if (this.verbose_ > 0) {
       console.log('Training PocketPLA model ...');
     }
     this._initWeights(initWeights, x[0].length + 1);
@@ -40,10 +89,12 @@ export default class PocketPLA extends Perceptron {
       console.log(`inputs: ${xVectors}, ${yEncoded}`);
     }
 
+    const maxPLAIter = Math.min(this._countMaxPLAIter(xVectors, yEncoded), maxIter);
+
     // update weights
     let resolved = false;
     while (this.iters_ < maxIter) {
-      if ((this.iters_ % 1000) === 0) {
+      if ((this.iters_ % 1000) === 0 && this.iters_ > 0) {
         console.log(`iteration: ${this.iters_}/${maxIter}`);
       }
       if (this._updateWeights(xVectors, yEncoded) === false) {
